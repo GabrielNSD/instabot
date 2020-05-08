eef83850e4ab2e44c6045e65064fc59c817b89b5
Gabriel Dantas <gabrielnsd@protonmail.com>
2020-05-07 23:35:50 -0300

    public commit

--- bot1.py
+++ bot1.py
@@ -21,10 +21,7 @@ def scroll_box(cosia, element):
     last_height = driver.execute_script("return document.body.scrollHeight")
     count=0
     while True:
-        if count > 1000:
-            SCROLL_PAUSE_TIME = 1.5
-        if count > 2000:
-            SCROLL_PAUSE_TIME = 2
+
         # Scroll down to bottom
         #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);", scr1)
         driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scr1)
@@ -83,10 +80,11 @@ time.sleep(5)
 
 driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]').click() #fechar pop up de notificações
 
-driver.get('https://www.instagram.com/morgsq/')
+
 
 time.sleep(2)
-#driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[5]/a').click() #abrir perfil
+
+driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[5]/a').click() #abrir perfil
 
 time.sleep(2)
 
@@ -95,7 +93,7 @@ str_n_seguindo = driver.find_element_by_xpath('//*[@id="react-root"]/section/mai
 
 n_seguidores = int(str_n_seguidores.replace('.',''))
 n_seguindo = int(str_n_seguindo.replace('.',''))
-'''
+
 driver.find_element_by_xpath(seguidores).click() #abrir janela de seguidores
 
 time.sleep(3)
@@ -126,7 +124,7 @@ action = webdriver.common.action_chains.ActionChains(driver)
 action.move_to_element_with_offset(followers_box, -50, 0)
 action.click()
 action.perform()
-'''
+
 
 driver.find_element_by_xpath(seguindo).click() #abrir janela de seguindo
 
